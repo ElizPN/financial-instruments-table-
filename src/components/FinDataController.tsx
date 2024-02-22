8580d97c809ce9a989d60af0be404d45bb90afa3
ElizPN <pankevych.liza@gmail.com>
2024-02-22 09:44:06 +0100

    fix prettier

--- src/components/FinDataController.tsx
+++ src/components/FinDataController.tsx
@@ -1,15 +1,32 @@
-import {finData} from "../configurations/fin-data.ts";
+import { finData } from "../configurations/fin-data.ts";
+import { priceSorter } from "../utils/priceSorter.ts";
 
-
+export interface FinDataItem {
+  ticker: string;
+  price: number;
+  assetClass: string;
+}
 export function FinDataController() {
+  const handleOnclickPrice = () => {
+    return priceSorter(finData);
+  };
 
-
-    return <div>
-        <h1>Fin data</h1>
-        <ul>
-      {finData.map((item: {ticker: string,price: number, assetClass: string })=>{
-        return <li> Ticker: {item.ticker}, Price: {item.price}, Asset Class: {item.assetClass}</li>
-      })}
+  return (
+    <div>
+      <h1>Fin data</h1>
+      <ul>
+        {finData.map((item: FinDataItem) => {
+          return (
+            <li key={item.ticker}>
+              {" "}
+              Ticker: {item.ticker}, Price: {item.price}, Asset Class:{" "}
+              {item.assetClass}
+            </li>
+          );
+        })}
       </ul>
+
+      <button onClick={handleOnclickPrice}>sort by price</button>
     </div>
+  );
 }
