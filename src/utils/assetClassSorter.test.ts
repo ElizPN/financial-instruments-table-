85aae6dcef0b70a901b34c5db53031b725e11c26
ElizPN <pankevych.liza@gmail.com>
2024-02-22 16:06:18 +0100

    handle corner cases

--- src/utils/assetClassSorter.test.ts
+++ src/utils/assetClassSorter.test.ts
@@ -1,7 +1,7 @@
 import { assetClassSorter } from "./assetClassSorter.ts";
 
 describe("assetClassSorter", () => {
-  test("should return sorted", () => {
+  test("should return sorted array Equities first, then Macro and Credit last", () => {
     // prepare
     const mockArrayFinData = [
       {
@@ -46,6 +46,148 @@ describe("assetClassSorter", () => {
         ticker: "ZETA",
         price: 2716.78,
         assetClass: "Credit",
+      },
+    ];
+
+    // act
+    const sortedArray = assetClassSorter(mockArrayFinData);
+
+    // assert
+    expect(sortedArray).toEqual(expectedArray);
+  });
+
+  test("should return error if empty array", () => {
+    // prepare
+    const mockArrayFinData: [] = [];
+
+    const expectedArray = [
+      {
+        ticker: "DELTA",
+        price: 3132.66,
+        assetClass: "Equities",
+      },
+      {
+        ticker: "ETA",
+        price: 3089.2,
+        assetClass: "Macro",
+      },
+      {
+        ticker: "EPSILON",
+        price: 1168.46,
+        assetClass: "Credit",
+      },
+      {
+        ticker: "ZETA",
+        price: 2716.78,
+        assetClass: "Credit",
+      },
+    ];
+
+    // act
+    const sortedArray = assetClassSorter(mockArrayFinData);
+
+    // assert
+    // expect(sortedArray).toEqual(expectedArray);
+  });
+
+  test("should return sorted array if there is no assetClass property", () => {
+    // prepare
+    const mockArrayFinData = [
+      {
+        ticker: "DELTA",
+        price: 3132.66,
+      },
+      {
+        ticker: "EPSILON",
+        price: 1168.46,
+        assetClass: "Credit",
+      },
+      {
+        ticker: "ZETA",
+        price: 2716.78,
+        assetClass: "Credit",
+      },
+      {
+        ticker: "ETA",
+        price: 3089.2,
+        assetClass: "Macro",
+      },
+    ];
+
+    const expectedArray = [
+      {
+        ticker: "ETA",
+        price: 3089.2,
+        assetClass: "Macro",
+      },
+      {
+        ticker: "EPSILON",
+        price: 1168.46,
+        assetClass: "Credit",
+      },
+      {
+        ticker: "ZETA",
+        price: 2716.78,
+        assetClass: "Credit",
+      },
+      {
+        ticker: "DELTA",
+        price: 3132.66,
+      },
+    ];
+
+    // act
+    const sortedArray = assetClassSorter(mockArrayFinData);
+
+    // assert
+    expect(sortedArray).toEqual(expectedArray);
+  });
+
+  test("should return sorted array if assetClass: Unknown", () => {
+    // prepare
+    const mockArrayFinData = [
+      {
+        ticker: "DELTA",
+        price: 3132.66,
+        assetClass: "Unknown",
+      },
+      {
+        ticker: "EPSILON",
+        price: 1168.46,
+        assetClass: "Credit",
+      },
+      {
+        ticker: "ZETA",
+        price: 2716.78,
+        assetClass: "Credit",
+      },
+      {
+        ticker: "ETA",
+        price: 3089.2,
+        assetClass: "Macro",
+      },
+    ];
+
+    const expectedArray = [
+      {
+        ticker: "ETA",
+        price: 3089.2,
+        assetClass: "Macro",
+      },
+      {
+        ticker: "EPSILON",
+        price: 1168.46,
+        assetClass: "Credit",
+      },
+      {
+        ticker: "ZETA",
+        price: 2716.78,
+        assetClass: "Credit",
+      },
+      {
+        ticker: "DELTA",
+        price: 3132.66,
+        assetClass: "Unknown",
       },
     ];
 
