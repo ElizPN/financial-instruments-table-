3f4166148ec3f1aef36a49a5798842f17ac5c6a9
ElizPN <pankevych.liza@gmail.com>
2024-02-23 19:01:07 +0100

    add initial ui

--- src/components/FinDataController.tsx
+++ src/components/FinDataController.tsx
@@ -1,32 +1,70 @@
 import { finData } from "../configurations/fin-data.ts";
+import { assetClassSorter } from "../utils/assetClassSorter.ts";
 import { priceSorter } from "../utils/priceSorter.ts";
+import { tickerSorter } from "../utils/tickerSorter.ts";
+import { Box, Container, Grid } from "@mui/material";
 
 export interface FinDataItem {
-  ticker: string;
-  price: number;
-  assetClass: string;
+  ticker?: string;
+  price?: number;
+  assetClass?: string;
 }
 export function FinDataController() {
   const handleOnclickPrice = () => {
+    console.log(priceSorter(finData));
+
     return priceSorter(finData);
   };
 
-  return (
-    <div>
-      <h1>Fin data</h1>
-      <ul>
-        {finData.map((item: FinDataItem) => {
-          return (
-            <li key={item.ticker}>
-              {" "}
-              Ticker: {item.ticker}, Price: {item.price}, Asset Class:{" "}
-              {item.assetClass}
-            </li>
-          );
-        })}
-      </ul>
+  const handleOnclickAssetClass = () => {
+    console.log(assetClassSorter(finData));
 
+    return assetClassSorter(finData);
+  };
+  const handleOnclickTickerSorter = () => {
+    console.log(tickerSorter(finData));
+
+    return tickerSorter(finData);
+  };
+
+  return (
+    <Container maxWidth="lg">
+      <Box
+        sx={{
+          margin: "2rem auto",
+          border: "1px solid lightgray",
+          borderRadius: "4px",
+        }}
+      >
+        <Grid container columns={3} spacing={1}>
+          <Grid item xs={1} sx={{ fontWeight: "bold" }}>
+            Ticker
+          </Grid>
+          <Grid item xs={1} sx={{ fontWeight: "bold" }}>
+            Price
+          </Grid>
+          <Grid item xs={1} sx={{ fontWeight: "bold" }}>
+            Asset Class
+          </Grid>
+
+          {finData.map((item: FinDataItem) => (
+            <>
+              <Grid item xs={1} key={item.ticker}>
+                {item.ticker}
+              </Grid>
+              <Grid item xs={1}>
+                {item.price}
+              </Grid>
+              <Grid item xs={1}>
+                {item.assetClass}
+              </Grid>
+            </>
+          ))}
+        </Grid>
+      </Box>
       <button onClick={handleOnclickPrice}>sort by price</button>
-    </div>
+      <button onClick={handleOnclickAssetClass}>sort by asset Credit</button>
+      <button onClick={handleOnclickTickerSorter}>sort by asset Ticker</button>
+    </Container>
   );
 }
