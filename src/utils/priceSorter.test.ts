3510ba4302cf4bb4c2cb2e08f7383fb1d5dfec2f
ElizPN <pankevych.liza@gmail.com>
2024-02-23 11:15:37 +0100

    handle corner cases with emty array and without property "price"

--- src/utils/priceSorter.test.ts
+++ src/utils/priceSorter.test.ts
@@ -45,4 +45,63 @@ describe("priceSorter", () => {
     // assert
     expect(sortedArray).toEqual(expectedArray);
   });
+
+  test("should return empty array when get empty array", () => {
+    // prepare
+    const mockArrayFindata: [] = [];
+
+    const expectedArray: [] = [];
+
+    // act
+    const sortedArray = priceSorter(mockArrayFindata);
+
+    // assert
+    expect(sortedArray).toEqual(expectedArray);
+  });
+
+  test("should return sorted array if there is no price property", () => {
+    // prepare
+    const mockArrayFindata = [
+      {
+        ticker: "BETA",
+        assetClass: "Equities",
+      },
+      {
+        ticker: "GAMMA",
+        price: 2299.1,
+        assetClass: "Equities",
+      },
+      {
+        ticker: "DELTA",
+        price: 3132.66,
+        assetClass: "Equities",
+      },
+    ];
+
+    const expectedArray = [
+      {
+        ticker: "DELTA",
+        price: 3132.66,
+        assetClass: "Equities",
+      },
+      {
+        ticker: "GAMMA",
+        price: 2299.1,
+        assetClass: "Equities",
+      },
+      {
+        ticker: "BETA",
+        assetClass: "Equities",
+      },
+    ];
+
+    // act
+    const sortedArray = priceSorter(mockArrayFindata);
+
+    // assert
+    expect(sortedArray).toEqual(expectedArray);
+  });
+
+
+  
 });
--- src/utils/priceSorter.ts
+++ src/utils/priceSorter.ts
@@ -1,7 +1,17 @@
 import { FinDataItem } from "../components/FinDataController";
 
 function compareNumbers(a: FinDataItem, b: FinDataItem) {
-  return b.price - a.price;
+  if (b.price && a.price) {
+    return b.price - a.price;
+  }
+  if (b.price === undefined && a.price !== undefined) {
+    return -1;
+  }
+  if (b.price !== undefined && a.price === undefined) {
+    return 1;
+  }
+
+  return 0;
 }
 
 export const priceSorter = (array: FinDataItem[]) => {
