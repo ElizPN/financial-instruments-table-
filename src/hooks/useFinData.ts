b7c33fb33e1ef3db8a7cf120c905751f29249f11
ElizPN <pankevych.liza@gmail.com>
2024-02-24 12:19:39 +0100

    add functionality to change color

--- src/hooks/useFinData.ts
+++ src/hooks/useFinData.ts
@@ -9,7 +9,29 @@ export interface FinDataItem {
   price?: number;
   assetClass?: string;
 }
+export interface FinDataItemColored extends FinDataItem {
+  priceColor: string;
+  rowColor: string;
+}
+
 const service = getFinDataService();
+
+export function getPriceColor(price?: number) {
+  return price && price > 0 ? "#00008B" : "red";
+}
+
+export function getRowColor(assetClass?: string) {
+  switch (assetClass) {
+    case "Macro":
+      return "white";
+    case "Equities":
+      return "#89CFF0";
+    case "Credit":
+      return "green";
+    default:
+      return "#D3D3D3";
+  }
+}
 
 export const useFinData = () => {
   const [finData, setFinData] = useState<FinDataItem[]>([]);
@@ -37,8 +59,21 @@ export const useFinData = () => {
     setFinData([...sortedFinData]);
   };
 
+  const finDataItemColored: FinDataItemColored[] = finData.map((item) => {
+    const priceColor = getPriceColor(item.price);
+    const rowColor = getRowColor(item.assetClass);
+
+    const coloredItem: FinDataItemColored = {
+      ...item,
+      priceColor,
+      rowColor,
+    };
+
+    return coloredItem;
+  });
+
   return {
-    finData,
+    finDataItemColored,
     handleOnclickPrice,
     handleOnclickAssetClass,
     handleOnclickTickerSorter,
