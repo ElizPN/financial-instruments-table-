e870f84f183ba97c2a531f23297189579d1fe391
ElizPN <pankevych.liza@gmail.com>
2024-02-25 16:00:23 +0100

    fetch findata from fake api

--- src/services/finDataService.ts
+++ src/services/finDataService.ts
@@ -1,14 +1,21 @@
-import { finData } from "../configurations/fin-data";
+import axios from "axios";
 import { FinDataItem } from "../hooks/useFinData";
+
+const apiURL = "http://localhost:3000/finData.json";
 
 type FinDataServiceType = {
   getData: () => Promise<FinDataItem[]>;
 };
-// function that creates and returns service with fake data
+// function that creates and returns service for fetching data
 export const getFinDataService = () => {
   const service: FinDataServiceType = {
-    getData: () => {
-      return Promise.resolve(finData);
+    getData: async () => {
+      try {
+        const response = await axios.get(apiURL);
+        return response.data;
+      } catch (error) {
+        throw error;
+      }
     },
   };
 
